1411e266e710aadb7e8c594dfe58d4e449381d6c
Xennis <code@xennis.org>
2016-10-03 16:11:44 +0200

    Set version to 1.0.0

--- awesome-cluster-map.php
+++ awesome-cluster-map.php
@@ -3,7 +3,7 @@
 Plugin Name: Awesome Cluster Map
 Plugin URI: https://github.com/XennisBlog/awesome-cluster-map
 Description: Create awesome marker cluster maps with OpenStreetMap (Leaflet)
-Version: 0.0.1
+Version: 1.0.0
 Author: Xennis
 Text Domain: awesome-cluster-map
 */
